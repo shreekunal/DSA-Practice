7ad2698c0df2269cad6b81d4618449e856228fc7
Kunal <shrii.kunal@gmail.com>
2025-03-05 23:05:19 +0530

    Time: 6 ms (45.84%), Space: 42.4 MB (78.74%) - LeetHub

--- 2235-capitalize-the-title/2235-capitalize-the-title.java
+++ 2235-capitalize-the-title/2235-capitalize-the-title.java
@@ -3,11 +3,11 @@ class Solution {
         String[] words = title.toLowerCase().split("\\s+");
         StringBuilder ans = new StringBuilder();
         for(int i = 0; i < words.length; i++){
-            if(words[i].length()<3){
-                ans.append(words[i]);
-            } else {
+            if(words[i].length()>2){
                 ans.append(Character.toUpperCase(words[i].charAt(0)));
                 ans.append(words[i].substring(1));
+            } else {
+                ans.append(words[i]);
             }
             if(i<words.length - 1) ans.append(" ");
         }
