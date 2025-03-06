4b0e6e1937d02775afbd718cada0b36c76c35594
Kunal <shrii.kunal@gmail.com>
2025-03-06 09:55:20 +0530

    Time: 1 ms (58.72%), Space: 40.6 MB (36.29%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,15 +1,15 @@
 class Solution {
     public int totalMoney(int n) {
-        int sum = 0, i = 1, j = 1;
-        while (i <= n) {
-            int k = j;
-            for (int w = 1; w <= 7 && i<=n; w++) {
-                sum += k;
-                k++;
-                i++;
+        int savings = 0, day = 1, start = 1;
+        while (day <= n) {
+            int dailyAmnt = start;
+            for (int w = 1; w <= 7 && day <= n; w++) {
+                savings += dailyAmnt;
+                dailyAmnt++;
+                day++;
             }
-            j++;
+            start++;
         }
-        return sum;
+        return savings;
     }
 }
