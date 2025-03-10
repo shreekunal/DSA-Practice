4722232c364f6c0ddcaf14bd6db61abbbbce063e
Kunal <shrii.kunal@gmail.com>
2025-03-10 15:31:39 +0530

    Time: 4 ms (26.69%), Space: 41.9 MB (73.92%) - LeetHub

---
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        String str[] = s.split("\\s+");
+        for (int i = 0; i < str.length && i < k; i++) {
+            sb.append(str[i]);
+            if (i < k - 1)
+                sb.append(" ");
+        }
+        return sb+"";
+    }
+}
