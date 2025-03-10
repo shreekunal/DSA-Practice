4cca28e0a43be4da65b8ebf647035397f19ba825
Kunal <shrii.kunal@gmail.com>
2025-03-10 15:32:56 +0530

    Time: 1 ms (82.75%), Space: 41.9 MB (64.27%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -1,7 +1,7 @@
 class Solution {
     public String truncateSentence(String s, int k) {
         StringBuilder sb = new StringBuilder();
-        String str[] = s.split("\\s+");
+        String str[] = s.split(" ");
         for (int i = 0; i < str.length && i < k; i++) {
             sb.append(str[i]);
             if (i < k - 1)
